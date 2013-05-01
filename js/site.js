83f214bb547b6b2057d82a4af47e0a0ffe5a0abd
Ian Dees <ian.dees@gmail.com>
2013-04-30 20:54:18 -0500

    Say something when the autocomplete is activated.

--- js/site.js
+++ js/site.js
@@ -16,7 +16,10 @@ $('#add-clue').click(function() {
     var new_clue = $("#clues li#clue-template").clone();
 
     var autocomplete = new google.maps.places.Autocomplete($(new_clue).find('#clue-address').get(0));
-
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        console.log("Clue is now " + place.geometry.location);
+    });
 
     $(new_clue).removeClass('hide');
     $(new_clue).appendTo('#clues');
