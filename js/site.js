6dd097ebc129820046fd356cbed1e9292ec0da81
Ian Dees <ian.dees@gmail.com>
2013-04-30 20:48:00 -0500

    Use Google Maps for the autocomplete thing.

--- js/site.js
+++ js/site.js
@@ -1,11 +1,23 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: new google.maps.LatLng(41.893, -87.645),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+});
 
-var bing = new L.BingLayer("Arzdiw4nlOJzRwOz__qailc8NiR31Tt51dN2D7cm57NrnceZnCpgOkmJhNpGoppU", "Aerial");
-map.addLayer(bing);
+var input = /** @type {HTMLInputElement} */(document.getElementById('clue-address'));
+var autocomplete = new google.maps.places.Autocomplete(input);
 
-var clue_tmpl = _.template(document.getElementById('clue-template').innerHTML);
+var clues_data = [];
 
 $('#add-clue').click(function() {
+    clues_data.push({clue:null, address:null, lat:null, lon:null, solved:false, assigned_to:null});
+
     console.log('Adding a clue.');
-    $('#clues').add(clue_tmpl({clue: {text: "This is dummy text"}}));
+    var new_clue = $("#clues li#clue-template").clone();
+
+    var autocomplete = new google.maps.places.Autocomplete($(new_clue).find('#clue-address').get(0));
+
+
+    $(new_clue).removeClass('hide');
+    $(new_clue).appendTo('#clues');
 });
