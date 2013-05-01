7e56d4dc848e85be8752e59579d86da93d49b936
Ian Dees <ian.dees@gmail.com>
2013-04-30 21:53:53 -0500

    Show a marker when a location is selected and toggle it's icon too.

--- js/site.js
+++ js/site.js
@@ -12,6 +12,12 @@ var clues_data = [];
 $('#add-clue').click(function() {
     var new_clue = $("#clues li.template").clone();
     new_clue.clue_number = $('#clues').children().length;
+    new_clue.marker = new google.maps.Marker({
+        map: map
+    });
+    new_clue.marker.setIcon(/** @type {google.maps.Icon} */({
+      url: 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+new_clue.clue_number+'%7CFFAA00%7C000000'
+    }));
     $(new_clue).removeClass('hide').removeClass('template');
     $(new_clue).appendTo('#clues');
 
@@ -20,15 +26,27 @@ $('#add-clue').click(function() {
     $(new_clue).find('#solved').click(function(){
         if($(this).is(':checked')){
             $(new_clue).find('#number img').attr('src', 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+new_clue.clue_number+'%7CAADD00%7C000000');
+            new_clue.marker.setIcon(/** @type {google.maps.Icon} */({
+              url: 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+new_clue.clue_number+'%7CAADD00%7C000000'
+            }));
         } else {
             $(new_clue).find('#number img').attr('src', 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+new_clue.clue_number+'%7CFFAA00%7C000000');
+            new_clue.marker.setIcon(/** @type {google.maps.Icon} */({
+              url: 'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+new_clue.clue_number+'%7CFFAA00%7C000000'
+            }));
         }
     });
 
-    var new_clue_dom_elem = $(new_clue).find('#clue-address').get(0);
-    var autocomplete = new google.maps.places.Autocomplete(new_clue_dom_elem);
+    var addr_dom_elem = $(new_clue).find('#clue-address').get(0);
+    var autocomplete = new google.maps.places.Autocomplete(addr_dom_elem);
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var place = autocomplete.getPlace();
-        console.log("Clue " + clue_number + " is now " + place.geometry.location);
+        if (!place.geometry) {
+            console.log("Clue " + new_clue.clue_number + " is not found.");
+            new_clue.marker.setVisible(false);
+        } else {
+            new_clue.marker.setPosition(place.geometry.location);
+            new_clue.marker.setVisible(true);
+        }
     });
 });
